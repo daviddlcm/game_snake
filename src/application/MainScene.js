98802f9b32de7aea14a54ad16acb3090c3b51e1d
david <daviddelacruzmorales42@gmail.com>
2024-09-23 10:54:40 -0600

    update

--- src/application/MainScene.js
+++ src/application/MainScene.js
@@ -1,86 +1,89 @@
 import { Snake } from '../domain/Snake.js';
 import { Food } from '../domain/Food.js';
-import { Board } from '../domain/Board.js';
 import { Block } from '../domain/Block.js';
+import { Board } from '../domain/Board.js';
 
 export class MainScene extends Phaser.Scene {
     constructor() {
         super({ key: 'MainScene' });
         this.snake = null;
         this.food = null;
+        this.block = null;
         this.cursors = null;
         this.isPaused = false;
-        this.timerWorker = null; 
-        this.foodWorker = null;  
+        this.timerWorker = null;
+        this.foodWorker = null;
+        this.blockWorker = null; 
         this.timerText = null;
     }
 
     preload() {
         this.load.image('food', 'assets/food.png');
         this.load.image('body', 'assets/body.png');
-        this.load.image('block', 'assets/block.png');
+        this.load.image('block', 'assets/block.png');  
     }
 
     create() {
         this.board = new Board(this);
         this.food = new Food(this, 3, 4);
         this.snake = new Snake(this, 8, 8);
+        
+        
+        this.createFoodWorker();
+        this.createBlockWorker();  
+        this.startTimer();  
 
-        
-        const validPositions = this.board.getValidLocations(this.snake.updateGrid(this.board.createGrid()));
-        const randomPosition = validPositions[Math.floor(Math.random() * validPositions.length)];
-        this.block = new Block(this, randomPosition.x, randomPosition.y);
-        
         this.timerText = this.add.text(0, 0, '00:00', {
             fontSize: '32px',
             fill: '#ffffff'
         });
         this.centerTimerText();
 
-       
-        this.startTimer();
-
-       
-        this.createFoodWorker();
-
-        
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        
         const pauseButton = document.getElementById('pauseButton');
         pauseButton.addEventListener('click', () => {
             this.togglePause(pauseButton);
         });
     }
 
+    createFoodWorker() {
+        if (typeof(Worker) !== "undefined") {
+            this.foodWorker = new Worker('./infrastructure/workers/foodWorker.js');
+            this.foodWorker.onmessage = (e) => {
+                const { action, position } = e.data;
+                if (action === "repositioned" && position) {
+                    this.food.setPosition(position.x, position.y);  
+                }
+            };
+        }
+    }
+
+    createBlockWorker() {
+        if (typeof(Worker) !== "undefined") {
+            this.blockWorker = new Worker('./infrastructure/workers/blockWorker.js');
+            const validLocations = this.board.getValidLocations(this.snake.updateGrid(this.board.createGrid()));
+
+            this.blockWorker.postMessage({ action: "reposition", data: { validLocations } });
+
+            this.blockWorker.onmessage = (e) => {
+                const { action, position } = e.data;
+                if (action === "repositioned" && position) {
+                    this.block = new Block(this, position.x, position.y);
+                }
+            };
+        }
+    }
+
     startTimer() {
         if (typeof(Worker) !== "undefined") {
             this.timerWorker = new Worker('./infrastructure/workers/timerWorker.js');
-            this.timerWorker.postMessage({ action: "start" });
-
+            this.timerWorker.postMessage({ action: "start" });  
             this.timerWorker.onmessage = (e) => {
                 const { minutes, seconds } = e.data;
                 const formattedTime = this.pad(minutes) + ':' + this.pad(seconds);
                 this.timerText.setText(formattedTime);
                 this.centerTimerText();
-            };
-        }
-    }
-
-    createFoodWorker() {
-        if (typeof(Worker) !== "undefined") {
-            this.foodWorker = new Worker('./infrastructure/workers/foodWorker.js');
-
-            this.foodWorker.onmessage = (e) => {
-                const { action, position, total } = e.data;
-
-                if (action === "repositioned") {
-                    if (position) {
-                        this.food.setPosition(position.x, position.y); 
-                    }
-                } else if (action === "eaten") {
-                    this.food.total = total; 
-                }
             };
         }
     }
@@ -91,7 +94,7 @@ export class MainScene extends Phaser.Scene {
             this.timerWorker.postMessage({ action: "pause" });
             button.textContent = 'Reanudar';
         } else {
-            this.timerWorker.postMessage({ action: "resume" });
+            this.timerWorker.postMessage({ action: "resume" });  
             button.textContent = 'Pausa';
         }
     }
@@ -102,7 +105,6 @@ export class MainScene extends Phaser.Scene {
 
     centerTimerText() {
         const gameWidth = this.game.config.width;
-        const gameHeight = this.game.config.height;
         this.timerText.x = gameWidth / 2 - this.timerText.width / 2;
     }
 
@@ -129,21 +131,19 @@ export class MainScene extends Phaser.Scene {
                 this.foodWorker.postMessage({ action: "eat" });
                 this.repositionFood();
             }
-    
-            
-            if (this.snake.head.x === this.block.block.x && this.snake.head.y === this.block.block.y) {
+
+            if (this.block && this.snake.head.x === this.block.block.x && this.snake.head.y === this.block.block.y) {
                 this.snake.alive = false;
             }
         }
     }
     
-    
 
     repositionFood() {
         const testGrid = this.board.createGrid();
         this.snake.updateGrid(testGrid);
-
         const validLocations = this.board.getValidLocations(testGrid);
+
         if (validLocations.length > 0) {
             this.foodWorker.postMessage({ action: "reposition", data: { validLocations } });
         }
@@ -155,6 +155,9 @@ export class MainScene extends Phaser.Scene {
         }
         if (this.foodWorker) {
             this.foodWorker.terminate();
+        }
+        if (this.blockWorker) {
+            this.blockWorker.terminate();
         }
     }
 }
--- src/infrastructure/workers/blockWorker.js
+++ src/infrastructure/workers/blockWorker.js
@@ -1,28 +1,12 @@
-onmessage = function(e) {
+// BlockWorker.js
+self.addEventListener("message", (e) => {
     const { action, data } = e.data;
 
-    if (action === "setBlockPosition") {
-        const { validPositions } = data;
-
-        if (validPositions && validPositions.length > 0) {
-            const randomPositions = [];
-            while (randomPositions.length < 2) { 
-                const randomIndex = Math.floor(Math.random() * validPositions.length);
-                const randomPosition = validPositions[randomIndex];
-                if (!randomPositions.some(pos => pos.x === randomPosition.x && pos.y === randomPosition.y)) {
-                    randomPositions.push(randomPosition);
-                }
-            }
-
-            postMessage({
-                action: "blockPositions",
-                positions: randomPositions
-            });
-        } else {
-            postMessage({
-                action: "error",
-                message: "No valid positions available for the blocks."
-            });
+    if (action === "reposition") {
+        const { validLocations } = data;
+        if (validLocations && validLocations.length > 0) {
+            const pos = validLocations[Math.floor(Math.random() * validLocations.length)];
+            self.postMessage({ action: "repositioned", position: pos });
         }
     }
-};
+});
